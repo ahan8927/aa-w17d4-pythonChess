58545889be9f853e3ef9a2e37d08a609c0c5dab9
Aaron <aaron.hanson.brb@gmail.com>
2020-12-03 15:37:59 -0700

    Phase 1 Done

--- tree.py
+++ tree.py
@@ -12,17 +12,17 @@ class Node:
     def children(self):
         return self._children
 
-    def add_child(self, node, call_parent = False):
+    def add_child(self, node, call_parent=False):
         if (node not in self._children):
             self._children.append(node)
             if not call_parent:
                 node.parent = self
 
-
-    def remove_child(self, node):
+    def remove_child(self, node, call_parent=False):
         if node != None:
             self._children.remove(node)
-            node.parent = None
+            if not call_parent:
+                node.parent = None
 
     @property
     def parent(self):
@@ -34,14 +34,39 @@ class Node:
             self._parent = parent
             return
         if self._parent:
-            self._parent.remove_child(self)
+            self._parent.remove_child(self, True)
         self._parent = parent
         parent.add_child(self, True)
 
+    def depth_search(self, value):
+        # print(self, self.value, value)
+        if self.value == value:
+            return self
+
+        if not self._children:
+            return
+        for child in self._children:
+            temp = child.depth_search(value)
+            if temp:
+                return temp
+        return None
+
+    def breadth_search(self, value):
+        queue.append(self)
+        while queue:
+            current_node = queue.pop(0)
+            if current_node.value == value:
+                return current_node
+            queue.extend(current_node.children)
+        return None
 
 
+# node1 = Node("root1")
+# node2 = Node("root2")
+# node3 = Node("root3")
 
+# node3.parent = node1
+# node3.parent = node2
 
-
-
-
+# print(node1.children)
+# print(node2.children)
